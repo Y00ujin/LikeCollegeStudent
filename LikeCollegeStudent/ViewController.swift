786fef4c28c905825c2723981ff0a73510db4ea3
y00ujin <yujngim43@gmail.com>
2021-02-22 00:28:42 +0900

    2021.02.22

--- LikeCollegeStudent/ViewController.swift
+++ LikeCollegeStudent/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myPageButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myPageButton.layer.cornerRadius = 18
     }
 
 
